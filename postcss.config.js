ff0d5c3df24cf78d7c2f27d2467f8f932ddfb657
d90375 <d90375@gmail.com>
2024-02-21 21:22:30 +0300

    remove cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,7 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    cssnano: {},
+    // cssnano: {},
     // ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
   },
 };
